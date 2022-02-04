b560917fb7b467885aa501b116248baa5178cd3c
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-03 21:33:09 -0500

    Time: 494 ms (31.27%), Space: 0B (100.00%) - LeetHub

--- 574-winning-candidate/574-winning-candidate.sql
+++ 574-winning-candidate/574-winning-candidate.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
-WITH w_function AS(
 SELECT 
-	id,
-    name,
-    COUNT(candidateId) AS num_votes
-FROM (SELECT c.id AS id, name, candidateId FROM candidate c LEFT JOIN vote v ON c.id = v.candidateId) AS temp 
-GROUP BY id)
-SELECT 
-	name
-FROM w_function
-WHERE num_votes = (SELECT MAX(num_votes) FROM w_function);
+    C.Name
+FROM Candidate C
+WHERE id = (SELECT CandidateId FROM Vote GROUP BY CandidateId ORDER BY COUNT(id) DESC LIMIT 1);
     
