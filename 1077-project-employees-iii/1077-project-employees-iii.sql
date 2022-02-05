baa38ec21a5494d0e0c10c60d1dae2e3c77dad1f
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-04 21:41:52 -0500

    Time: 726 ms (21.11%), Space: 0B (100.00%) - LeetHub

--- 1077-project-employees-iii/1077-project-employees-iii.sql
+++ 1077-project-employees-iii/1077-project-employees-iii.sql
@@ -1,27 +1,27 @@
 # Write your MySQL query statement below
-# SELECT 
-# 	project_id,
-#     p.employee_id
-# FROM project p
-# JOIN employee e
-# 	ON p.employee_id = e.employee_id
-# WHERE (project_id, experience_years) IN
-# (SELECT 
-# 	project_id,
-#     MAX(experience_years) AS max_years
-# FROM project a
-# JOIN employee b
-# 	ON a.employee_id = b.employee_id
-# GROUP BY 1);
+SELECT 
+	project_id,
+    p.employee_id
+FROM project p
+JOIN employee e
+	ON p.employee_id = e.employee_id
+WHERE (project_id, experience_years) IN
+(SELECT 
+	project_id,
+    MAX(experience_years) AS max_years
+FROM project a
+JOIN employee b
+	ON a.employee_id = b.employee_id
+GROUP BY 1);
 
 # method 2 with rank()
-SELECT 
-	project_id, 
-	employee_id
-FROM
-    (SELECT project_id,
-    p.employee_id,
-    RANK() OVER(PARTITION BY project_id ORDER BY experience_years DESC) AS my_rank
-    FROM Project p JOIN Employee e
-    ON p.employee_id = e.employee_id) t
-WHERE my_rank = 1;
+# SELECT 
+# 	project_id, 
+# 	employee_id
+# FROM
+#     (SELECT project_id,
+#     p.employee_id,
+#     RANK() OVER(PARTITION BY project_id ORDER BY experience_years DESC) AS my_rank
+#     FROM Project p JOIN Employee e
+#     ON p.employee_id = e.employee_id) t
+# WHERE my_rank = 1;
