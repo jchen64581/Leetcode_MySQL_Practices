4b3b3f05b25d6c8f4d8b993e424ec0b6f3125b5f
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-04 20:11:03 -0500

    Time: 1155 ms (51.78%), Space: 0B (100.00%) - LeetHub

--- 1069-product-sales-analysis-ii/1069-product-sales-analysis-ii.sql
+++ 1069-product-sales-analysis-ii/1069-product-sales-analysis-ii.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 SELECT 
-	p.product_id,
+	product_id,
     SUM(quantity) AS total_quantity
-FROM sales s
-JOIN product p
-	ON s.product_id = p.product_id
+FROM sales
 GROUP BY 1;
