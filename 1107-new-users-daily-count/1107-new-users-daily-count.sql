019037d5147d069fb3cda55e484affb69dc06430
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-05 21:41:47 -0500

    Time: 597 ms (28.99%), Space: 0B (100.00%) - LeetHub

--- 1107-new-users-daily-count/1107-new-users-daily-count.sql
+++ 1107-new-users-daily-count/1107-new-users-daily-count.sql
@@ -1,26 +1,26 @@
 # Write your MySQL query statement below
-WITH cte AS (
-SELECT
-	user_id,
-	MIN(activity_date) AS first_login
-FROM traffic
-WHERE activity = 'login'
-GROUP BY 1)
-SELECT 
-	first_login AS login_date,
-    COUNT(user_id) AS user_count
-FROM cte
-WHERE DATEDIFF('2019-06-30', first_login) <= 90
-GROUP BY 1;
-
-#method 2
-# SELECT 
-# 	login_date, 
-#     COUNT(*) AS user_count
-# FROM
-# (SELECT user_id, MIN(activity_date) AS login_date
+# WITH cte AS (
+# SELECT
+# 	user_id,
+# 	MIN(activity_date) AS first_login
 # FROM traffic
 # WHERE activity = 'login'
-# GROUP BY user_id) a
-# WHERE login_date BETWEEN DATE_ADD('2019-06-30', INTERVAL -90 DAY) AND '2019-06-30'
-# GROUP BY login_date;
+# GROUP BY 1)
+# SELECT 
+# 	first_login AS login_date,
+#     COUNT(user_id) AS user_count
+# FROM cte
+# WHERE DATEDIFF('2019-06-30', first_login) <= 90
+# GROUP BY 1;
+
+#method 2
+SELECT 
+	login_date, 
+    COUNT(*) AS user_count
+FROM
+(SELECT user_id, MIN(activity_date) AS login_date
+FROM traffic
+WHERE activity = 'login'
+GROUP BY user_id) a
+WHERE login_date BETWEEN DATE_ADD('2019-06-30', INTERVAL -90 DAY) AND '2019-06-30'
+GROUP BY login_date;
