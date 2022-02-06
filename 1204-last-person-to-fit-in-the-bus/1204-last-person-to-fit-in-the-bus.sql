fd23e01cbacc90997e1df97c727f429f5305d3f7
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-06 16:28:30 -0500

    Time: 769 ms (68.55%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,14 +1,27 @@
 # Write your MySQL query statement below
 WITH cte AS (
-SELECT 
-	turn,
-    person_id,
+SELECT
     person_name,
-    weight,
 	SUM(weight) OVER (ORDER BY turn) AS running_sum
 FROM queue 
-ORDER BY 1)
+ORDER BY turn)
 SELECT person_name FROM cte 
 WHERE running_sum <= 1000
 ORDER BY running_sum DESC
 LIMIT 1;
+
+
+
+# WITH cte AS (
+# SELECT 
+# 	turn,
+#     person_id,
+#     person_name,
+#     weight,
+# 	SUM(weight) OVER (ORDER BY turn) AS running_sum
+# FROM queue 
+# ORDER BY turn)
+# SELECT person_name FROM cte 
+# WHERE running_sum <= 1000
+# ORDER BY running_sum DESC
+# LIMIT 1;
