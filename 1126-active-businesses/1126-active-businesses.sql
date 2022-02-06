d6bdd07851dc0f7fda0166792d2a52d8ced0abcf
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-05 22:53:06 -0500

    Time: 1197 ms (12.56%), Space: 0B (100.00%) - LeetHub

---
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH cte AS (
+SELECT
+	business_id,
+	event_type,
+    occurences,
+    AVG(occurences) OVER (PARTITION BY event_type) AS avg_occurences,
+    CASE WHEN occurences > AVG(occurences) OVER (PARTITION BY event_type) THEN 'yes' ELSE 'no' END AS greater_than_avg
+FROM events)
+SELECT 
+	business_id
+FROM cte 
+WHERE greater_than_avg = 'yes'
+GROUP BY 1
+HAVING COUNT(event_type) > 1;
