edfc478e83055043514b96aa89570f976adffdc8
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-06 19:33:56 -0500

    Time: 745 ms (31.05%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+
 select
 	DISTINCT query_name,
     avg_quality_rate AS quality ,
@@ -6,10 +7,23 @@ select
 FROM
 (SELECT
 	query_name,
-    result,
-    position,
     rating,
-    rating/position AS quality_rate,
 	ROUND(AVG(rating/position) OVER (PARTITION BY query_name),2) AS avg_quality_rate
 FROM queries) AS temp
 GROUP BY 1;
+
+
+# select
+# 	DISTINCT query_name,
+#     avg_quality_rate AS quality ,
+#     ROUND((COUNT(CASE WHEN rating < 3 THEN 1 ELSE NULL END)/COUNT(*))*100,2) AS poor_query_percentage 
+# FROM
+# (SELECT
+# 	query_name,
+#     result,
+#     position,
+#     rating,
+#     rating/position AS quality_rate,
+# 	ROUND(AVG(rating/position) OVER (PARTITION BY query_name),2) AS avg_quality_rate
+# FROM queries) AS temp
+# GROUP BY 1;
