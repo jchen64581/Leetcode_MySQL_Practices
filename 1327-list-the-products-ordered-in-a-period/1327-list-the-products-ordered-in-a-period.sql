afb4c921d762dbe1f132dc3fd10d20fc8c798309
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-09 19:37:15 -0500

    Time: 1124 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -5,6 +5,6 @@ SELECT
 FROM products p
 LEFT JOIN orders o
 	ON p.product_id = o.product_id
-WHERE MONTH(order_date) = 02
+WHERE MONTH(order_date) = 02 AND YEAR(order_date) = 2020
 GROUP BY p.product_id
 HAVING SUM(unit) >= 100;
