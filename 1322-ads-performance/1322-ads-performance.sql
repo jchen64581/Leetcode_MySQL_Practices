cb211d543ac96d65b42399ba2e286988ad37f1a6
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-09 19:25:15 -0500

    Time: 567 ms (46.19%), Space: 0B (100.00%) - LeetHub

--- 1322-ads-performance/1322-ads-performance.sql
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -2,7 +2,7 @@
 SELECT
 	ad_id,
     ROUND(IFNULL((SUM(CASE WHEN action = 'clicked' THEN 1 ELSE 0 END)
-    /(SUM(CASE WHEN action = 'clicked' THEN 1 ELSE 0 END) + SUM(CASE WHEN action = 'viewed' THEN 1 ELSE 0 END))*100),0),2) AS ctr
+    /(SUM(CASE WHEN action = 'clicked' OR action = 'viewed' THEN 1 ELSE 0 END))*100),0),2) AS ctr
 FROM ads
 GROUP BY 1
-ORDER BY 2 DESC, 1;
+ORDER BY 2 DESC,1;
