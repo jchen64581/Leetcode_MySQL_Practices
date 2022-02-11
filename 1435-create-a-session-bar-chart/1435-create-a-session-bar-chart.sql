cc0e049c9071898bb5014aa7624cf1480818a879
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-10 21:54:34 -0500

    Time: 396 ms (59.73%), Space: 0B (100.00%) - LeetHub

--- 1435-create-a-session-bar-chart/1435-create-a-session-bar-chart.sql
+++ 1435-create-a-session-bar-chart/1435-create-a-session-bar-chart.sql
@@ -2,23 +2,23 @@
 (
 SELECT
 	'[0-5>' AS bin, 
-	SUM(CASE WHEN duration/60 BETWEEN 0 AND 5 THEN 1 ELSE 0 END)  AS total FROM sessions
+	SUM(duration/60 < 5)  AS total FROM sessions
 )
 UNION ALL
 (
 SELECT 
 	'[5-10>' AS bin,
-	SUM(CASE WHEN duration/60 BETWEEN 5 AND 10 THEN 1 ELSE 0 END)  AS total FROM sessions
+	SUM(duration/60 BETWEEN 5 AND 10)  AS total FROM sessions
 )
 UNION ALL
 (
 SELECT
 	'[10-15>' AS bin, 
-	SUM(CASE WHEN duration/60 BETWEEN 10 AND 15 THEN 1 ELSE 0 END)  AS total FROM sessions
+	SUM(duration/60 BETWEEN 10 AND 15 )  AS total FROM sessions
 )
 UNION ALL
 (
 SELECT 
 	'15 or more' AS bin,
-	SUM(CASE WHEN (duration/60) >= 15 THEN 1 ELSE 0 END)  AS total FROM sessions
+	SUM(duration/60 >= 15 )  AS total FROM sessions
 );
