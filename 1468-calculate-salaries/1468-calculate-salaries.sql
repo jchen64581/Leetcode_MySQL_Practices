4245c3f93aadbcd85a4d1bf0b74ba68ca5fd4e50
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-11 20:40:30 -0500

    Time: 653 ms (52.85%), Space: 0B (100.00%) - LeetHub

--- 1468-calculate-salaries/1468-calculate-salaries.sql
+++ 1468-calculate-salaries/1468-calculate-salaries.sql
@@ -1,18 +1,11 @@
 # Write your MySQL query statement below
-WITH cte AS (
-SELECT
-	company_id AS c_id,
-	MAX(salary) AS max_salary
-FROM  salaries 
-GROUP BY 1
-)
-SELECT
-	company_id,
-    employee_id,
+SELECT 
+	company_id, 
+    employee_id, 
     employee_name,
-    CASE 
-		WHEN (SELECT max_salary FROM cte WHERE company_id = c_id) < 1000 THEN ROUND(salary) 
-		WHEN (SELECT max_salary FROM cte WHERE company_id = c_id) <= 10000 THEN ROUND(salary*(1-0.24))
-		ELSE ROUND(salary*(1-0.49))
-	END AS salary
+    ROUND(CASE
+		WHEN MAX(salary) OVER (PARTITION BY company_id) <1000 THEN salary
+		WHEN MAX(salary) OVER (PARTITION BY company_id) <=10000 THEN salary*(1-0.24)
+        ELSE salary*(1-0.49)
+	END) AS salary
 FROM salaries;
