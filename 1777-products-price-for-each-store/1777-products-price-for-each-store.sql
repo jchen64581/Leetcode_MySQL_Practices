b5f4b62164a22c7b496b5d4f19fd030d97e905d8
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-26 16:17:58 -0500

    Time: 492 ms (66.45%), Space: 0B (100.00%) - LeetHub

--- 1777-products-price-for-each-store/1777-products-price-for-each-store.sql
+++ 1777-products-price-for-each-store/1777-products-price-for-each-store.sql
@@ -1,9 +1,17 @@
 # Write your MySQL query statement below
-SELECT
-	DISTINCT product_id AS product_id,
-    MAX(CASE WHEN store = 'store1' AND product_id = product_id THEN price ELSE NULL END) AS store1,
-	MAX(CASE WHEN store = 'store2' AND product_id = product_id THEN price ELSE NULL END) AS store2,
-    MAX(CASE WHEN store = 'store3' AND product_id = product_id THEN price ELSE NULL END) AS store3
-FROM products
-GROUP BY 1
-;
+# SELECT
+# 	DISTINCT product_id AS product_id,
+#     MAX(CASE WHEN store = 'store1'  THEN price ELSE NULL END) AS store1,
+# 	MAX(CASE WHEN store = 'store2'  THEN price ELSE NULL END) AS store2,
+#     MAX(CASE WHEN store = 'store3'  THEN price ELSE NULL END) AS store3
+# FROM products
+# GROUP BY 1
+# ;
+
+
+select product_id, 
+       sum(case when store='store1' then price end) as store1,
+       sum(case when store='store2' then price end) as store2,
+       sum(case when store='store3' then price end) as store3
+from Products
+group by product_id
