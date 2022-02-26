f1026ab222094e4cc56fcb0362ec730c9e5b105e
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-25 20:39:38 -0500

    Time: 864 ms (67.47%), Space: 0B (100.00%) - LeetHub

--- 1715-count-apples-and-oranges/1715-count-apples-and-oranges.sql
+++ 1715-count-apples-and-oranges/1715-count-apples-and-oranges.sql
@@ -1,17 +1,7 @@
 # Write your MySQL query statement below
-WITH cte AS (
-SELECT 
-	box_id,
-    b.chest_id,
-    b.apple_count AS b_apple_count,
-    b.orange_count  AS b_orange_count,
-    IFNULL(c.apple_count,0) AS c_apple_count,
-	IFNULL(c.orange_count,0) AS c_orange_count
-FROM boxes b
-LEFT JOIN chests c
-	ON b.chest_id = c.chest_id)
-SELECT 
-	SUM(b_apple_count) + SUM(c_apple_count) AS apple_count,
-    SUM(b_orange_count) + SUM(c_orange_count) AS orange_count
-FROM cte;	
-;
+select
+  sum(b.apple_count + ifnull(c.apple_count, 0)) as apple_count,
+  sum(b.orange_count + ifnull(c.orange_count,0)) as orange_count
+from Boxes b
+left join Chests c
+  on b.chest_id = c.chest_id
