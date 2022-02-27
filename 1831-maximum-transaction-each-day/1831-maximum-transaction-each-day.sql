0b59459e6df1d1eb1e2d3a1be5e0ca69a2f1c1b7
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-02-26 20:29:58 -0500

    Time: 969 ms (6.76%), Space: 0B (100.00%) - LeetHub

--- 1831-maximum-transaction-each-day/1831-maximum-transaction-each-day.sql
+++ 1831-maximum-transaction-each-day/1831-maximum-transaction-each-day.sql
@@ -1,13 +1,25 @@
 # Write your MySQL query statement below
-WITH cte AS (
-SELECT 
-	transaction_id,
-    day,
-    amount,
-    DENSE_RANK() OVER (PARTITION BY DATE_FORMAT(day, '%Y-%m-%d') ORDER BY amount DESC) AS amount_rank
-FROM transactions)
-SELECT
-	transaction_id 
-FROM cte
-WHERE amount_rank = 1
-ORDER BY transaction_id;
+# WITH cte AS (
+# SELECT 
+# 	transaction_id,
+#     day,
+#     amount,
+#     DENSE_RANK() OVER (PARTITION BY DATE_FORMAT(day, '%Y-%m-%d') ORDER BY amount DESC) AS amount_rank
+# FROM transactions)
+# SELECT
+# 	transaction_id 
+# FROM cte
+# WHERE amount_rank = 1
+# ORDER BY transaction_id;
+
+
+
+SELECT  transaction_id
+FROM
+(
+	SELECT  * , DENSE_RANK() OVER (PARTITION BY DATE(day) ORDER BY amount DESC) as dt
+	FROM    Transactions
+	ORDER BY day ASC
+) tmp
+WHERE dt = 1
+ORDER BY transaction_id ASC;
