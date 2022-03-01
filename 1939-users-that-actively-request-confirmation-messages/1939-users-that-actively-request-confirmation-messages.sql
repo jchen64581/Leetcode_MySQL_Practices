cae6afe3a53c14868f3a55e43603bbd73b99be7b
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-03-01 14:40:35 -0500

    Time: 1093 ms (10.69%), Space: 0B (100.00%) - LeetHub

--- 1939-users-that-actively-request-confirmation-messages/1939-users-that-actively-request-confirmation-messages.sql
+++ 1939-users-that-actively-request-confirmation-messages/1939-users-that-actively-request-confirmation-messages.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
+
 SELECT
-    s.user_id
---     c.time_stamp AS time_stamp ,
---     c1.time_stamp AS time_stamp1
+    DISTINCT s.user_id
 FROM signups s
 LEFT JOIN confirmations c
 	ON s.user_id = c.user_id
 LEFT JOIN confirmations c1
 	ON c.user_id = c1.user_id AND c.time_stamp < c1.time_stamp
-WHERE TIMESTAMPDIFF(SECOND, c.time_stamp, c1.time_stamp)  <= 86400
-GROUP BY s.user_id
-;
+WHERE TIMESTAMPDIFF(SECOND, c.time_stamp, c1.time_stamp)  <= 86400;
