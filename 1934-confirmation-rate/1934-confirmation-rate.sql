a8c1ea7897c078b140f097b134a73ab89df76615
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2022-03-01 14:09:12 -0500

    Time: 999 ms (10.12%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,12 +1,15 @@
 # Write your MySQL query statement below
-WITH cte AS (
-SELECT
-	s.user_id,
-    CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END AS confirmed_msg
-FROM signups s 
-LEFT JOIN confirmations c ON s.user_id = c.user_id)
-SELECT 
-	user_id,
-    ROUND(SUM(confirmed_msg)/COUNT(*),2) AS confirmation_rate 
-FROM cte
-GROUP BY 1;
+# SELECT 
+#   s.user_id,
+#   ROUND(AVG(CASE WHEN action = 'confirmed' THEN 1.00 ELSE 0.00 END),2) AS confirmation_rate
+# FROM Signups s LEFT JOIN Confirmations c ON s.user_id = c.user_id
+# GROUP BY s.user_id;
+
+
+select
+    s.user_id,
+    ifnull(round(sum(action='confirmed')/sum(action='confirmed' or action='timeout'), 2), 0) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
